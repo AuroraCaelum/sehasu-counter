a414d6b818168a4ed22c9de2abb071c047464c37
AuroraCaelum <ddugi1214@naver.com>
2023-08-05 22:44:31 +0800

    Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
 	import { t, locale, locales } from '$lib/i18n/i18n';
 	import './styles.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="app">
